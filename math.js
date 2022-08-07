2018bcec0d6fb1a4bc106fe0023911365a15eef2
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-06 22:47:04 -0500

    update notes abput class

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-// Square perimeter
+// Square
 const squareSide = 5;
 const squarePerimeter = squareSide * 4 + 'cm';
 const squareArea = squareSide * squareSide + 'cm2';
@@ -7,10 +7,11 @@ console.log({squareSide, squarePerimeter, squareArea})
 function squareCalcs(side){
     return{
         perimeter: side * 4 + 'cm',
-        area: side * side
-    }
-}
+        area: side * side + 'cm2'
+    };
+};
 
+//Triangle
 const triangleSide1 = 6;
 const triangleSide2 = 6;
 const triangleSideBase = 4;
@@ -22,6 +23,6 @@ console.log({triangleSide1, triangleSide2, triangleSideBase, triangleHeight,tria
 function triangleCalcs(side1, side2, base, height){
     return{
         perimeter: side1 + side2 + base + 'cm',
-        area: ( base * height ) / 2
-    }
-}
+        area: ( base * height ) / 2 + 'cm2'
+    };
+};
