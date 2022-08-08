9a327f32f7d9656ae5d8f3ab8ea7aad893da3366
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-08 15:18:35 -0500

    result of coupon validation

--- discounts.js
+++ discounts.js
@@ -2,13 +2,17 @@
 
 const inputPrice = document.querySelector('#price');
 const inputDiscount = document.querySelector('#discount');
-const calc = document.querySelector('#calc')
+const inputCoupon = document.querySelector('#coupon');
+const calcDiscount = document.querySelector('#calcDiscount')
+const applyCoupon = document.querySelector('#applyCoupon')
 const result = document.querySelector('#result')
+
+calcDiscount.addEventListener('click', finalPrice);
+applyCoupon.addEventListener('click', finalPriceCoupon);
 
 function finalPrice(){
     const price = Number(inputPrice.value);
     const discount = Number(inputDiscount.value);
-    console.log({price,discount})
 
     if(!price || !discount){
         result.innerHTML = 'Price and Discount is out range of values, please verify it and try again'
@@ -27,7 +31,37 @@ function finalPrice(){
     inputPrice.value = '';
     inputDiscount.value = '';
     result.innerHTML = 'Price with discount: $' + finalPrice 
-    
 };
 
-calc.addEventListener('click', finalPrice);
+
+function finalPriceCoupon(){
+    const price = Number(inputPrice.value);
+    const coupon = inputCoupon.value;
+    let discount;
+
+    if(coupon === 'ABC'){
+        discount = 30
+    } else if (coupon === 'DEF'){
+        discount = 45
+    } else {
+        discount = 0
+    }
+
+    if(!price || !discount){
+        result.innerHTML = 'Price and Discount is out range of values, please verify it and try again'
+        return
+    }
+    if(discount > 100){
+        result.innerHTML = 'Discount of ' + discount + '% is out range of discounts, please verify it and try again'
+        return
+    }
+    if(price <= 0){
+        result.innerHTML = 'Price ' + price + ' is out range of prices, please verify it and try again'
+        return
+    }
+    
+    const finalPrice = ( price * ( 100 - discount ) ) / 100;
+    inputPrice.value = '';
+    inputCoupon.value = '';
+    result.innerHTML = 'Price with discount: $' + finalPrice 
+};
